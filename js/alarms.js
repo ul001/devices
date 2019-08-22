5ff07068f149aebde1367a223b60c023c908c1f7
panhong <381500590@qq.com>
2019-08-22 14:39:40 +0800

    完成iOS报警对接

--- js/alarms.js
+++ js/alarms.js
@@ -4,30 +4,35 @@ var u = navigator.userAgent,
 var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //安卓系统
 var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios系统
 var menuId;
-if(isIOS){
-    menuId = "";
-}else{
+if (isIOS) {
+    window.webkit.messageHandlers.iOS.postMessage(null);
+    var storage = localStorage.getItem("accessToken");
+    storage = JSON.parse(storage);
+    menuId = storage.fmenuId;
+} else {
     menuId = android.getMenuId();
 }
 
-function loadMenu(){
+function loadMenu() {
     $(".content").empty();
-    Substation.getDataByAjax("/getSubinfoVoByPid",{pid:menuId},function(data){
-        if(data.hasOwnProperty("menuList")&&data.menuList.length>0){
-            $(data.menuList).each(function(){
+    Substation.getDataByAjax("/getSubinfoVoByPid", {
+        pid: menuId
+    }, function (data) {
+        if (data.hasOwnProperty("menuList") && data.menuList.length > 0) {
+            $(data.menuList).each(function () {
                 $(".content").append("<div class=\"content-block-title\"></div>\n" +
-                                     "            <div class=\"list-block\">\n" +
-                                     "                <ul>\n" +
-                                     "                    <li class=\"item-content item-link\" id=\""+this.fMenuid+"\" value=\""+this.fCode+"\">\n" +
-                                     "                        <div class=\"item-media\"><img src=\"img/alarmPic.png\" width=\"20\"></div>\n" +
-                                     "                        <div class=\"item-inner\">\n" +
-                                     "                            <div class=\"item-title\">"+this.fMenuname+"</div>\n" +
-                                     "                            <div class=\"item-after\" id=\""+this.fCode+"\"></div>\n" +
-                                     "                        </div>\n" +
-                                     "                    </li>\n" +
-                                     "                </ul>\n" +
-                                     "                <div class=\"list-block-label\"></div>\n" +
-                                     "            </div>");
+                    "            <div class=\"list-block\">\n" +
+                    "                <ul>\n" +
+                    "                    <li class=\"item-content item-link\" id=\"" + this.fMenuid + "\" value=\"" + this.fCode + "\">\n" +
+                    "                        <div class=\"item-media\"><img src=\"img/alarmPic.png\" width=\"20\"></div>\n" +
+                    "                        <div class=\"item-inner\">\n" +
+                    "                            <div class=\"item-title\">" + this.fMenuname + "</div>\n" +
+                    "                            <div class=\"item-after\" id=\"" + this.fCode + "\"></div>\n" +
+                    "                        </div>\n" +
+                    "                    </li>\n" +
+                    "                </ul>\n" +
+                    "                <div class=\"list-block-label\"></div>\n" +
+                    "            </div>");
             });
             fillData(0);
         }
@@ -42,7 +47,7 @@ function fillData(parentId) {
         $(data).each(function (key, value) {
             var name = value.name;
             if (name == "遥测越限") {
-                if($("#bianwei")){
+                if ($("#bianwei")) {
                     if (value.count > 0) {
                         var string = '<span class="badge">' + value.count + '</span>';
                         $("#bianwei").html(string);
@@ -50,7 +55,7 @@ function fillData(parentId) {
                     }
                 }
             } else if (name == "遥信变位") {
-                if($("#yuexian")){
+                if ($("#yuexian")) {
                     if (value.count > 0) {
                         var string = '<span class="badge">' + value.count + '</span>';
                         $("#yuexian").html(string);
@@ -59,7 +64,7 @@ function fillData(parentId) {
                 }
 
             } else if (name == "平台运行") {
-                if($("#platform")){
+                if ($("#platform")) {
                     if (value.count > 0) {
                         var string = '<span class="badge">' + value.count + '</span>';
                         $("#platform").html(string);
