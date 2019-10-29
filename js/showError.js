3b786fe7399f5096ad1fd186985532428793ce13
panhong <381500590@qq.com>
2019-10-29 13:02:53 +0800

    提交错误404的网页

---
+++ js/showError.js
@@ -0,0 +1,11 @@
+$(".back_btn").click(function () {
+    var u = navigator.userAgent,
+        app = navigator.appVersion;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //安卓系统
+    var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios系统
+    if (isIOS) {
+        window.webkit.messageHandlers.goBackiOS.postMessage("");
+    } else {
+        android.goBack();
+    }
+});
