3ab8f95a7e6f314c41554b8ec483074a72f642ad
panhong <381500590@qq.com>
2019-11-20 16:10:17 +0800

    解决iOS左滑底部tabbar消失问题

--- js/alarms.js
+++ js/alarms.js
@@ -13,6 +13,21 @@ if (isIOS) {
     menuId = android.getMenuId();
 }
 
+window.addEventListener('pageshow', function (e) {
+    //ios系统 返回页面 不刷新的问题 Safari内核缓存机制导致 方案一 方案二：设置meta标签，清除页面缓存
+    var u = navigator.userAgent,
+        app = navigator.appVersion;
+    var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+    if (e.persisted && isIOS) {
+        // var needUpdate = localStorage.getItem("need-update");
+        // if (needUpdate) {
+        //     localStorage.removeItem("need-update");
+        window.location.reload();
+        // }
+        window.webkit.messageHandlers.needHiddenTabbar.postMessage("NO");
+    }
+});
+
 function loadMenu() {
     $(".list-container").empty();
     $.showPreloader();
