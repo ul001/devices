cd21b32a1c873ec2e994ef1b82c16825181d1860
xuhang <925645182@qq.com>
2020-01-07 14:11:55 +0800

    light7冲突解决

--- js/userReport.js
+++ js/userReport.js
@@ -1,3 +1,7 @@
+$('#fullpage').fullpage({
+		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+		'navigation': true
+});
 var selectSubid = "";
 var clickSubid = "";
 if(selectSubid==""||$("#dateStart").val()==""||$("#dateEnd").val()==""){
