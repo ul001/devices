4d71c196022e0ecd91bcb445d38c7fb5abfd6473
panhong <381500590@qq.com>
2020-01-08 09:26:27 +0800

    light7方法无响应

--- js/CustomReport.js
+++ js/CustomReport.js
@@ -1756,6 +1756,162 @@ jQuery(document).ready(function () {
     //   $(".deletePage").remove();
     // }
     //   });
+
+    //添加右上角事件
+    $('#searchBtn').click(function () {
+        $(".close-panel").click();
+        if (saveParam != null) {
+            clickSubid = saveParam['fSubid'];
+            saveParam = null;
+        }
+        if ($("#search").val() == "") {
+            //        $("#subname").text("所有变电所");
+            selectSubid = "";
+        } else if (clickSubid != "") {
+            //        $("#subname").text($("#search").val());
+            selectSubid = clickSubid;
+            clickSubid = "";
+        }
+        getFirstPage();
+    });
+
+    $("#dateStart").calendar();
+    $("#dateEnd").calendar();
+    $("#listContainer").hide();
+
+    function getSomeSubstation() {
+        var url = "/getSubListByLetter";
+        var searchKey = $("#search").val();
+        var params = {
+            key: searchKey
+        }
+        $("#listContainer").empty();
+        Substation.getDataByAjaxNoLoading(url, params, function (data) {
+            $(data).each(function () {
+                $("#listContainer").append('<li class="item-content" data-id="' + this.fSubid + '">' +
+                    '<div class="item-inner">' +
+                    '<div class="item-title">' + this.fSubname + '</div>' +
+                    '</div>' +
+                    '</li>');
+            });
+            $("#listContainer").show();
+            $("#listContainer .item-content").unbind().click(function () {
+                clickSubid = $(this).attr("data-id");
+                var clickName = $(this).find(".item-title").text();
+                $("#search").val(clickName);
+                $("#listContainer").empty();
+                $("#listContainer").hide();
+                //            $("#subname").text(clickName);
+            });
+        });
+    }
+
+    $('#search').bind('keydown', function (event) {
+        if (event.keyCode == 13) {
+            getSomeSubstation();
+            document.activeElement.blur();
+        }
+    });
+
+    $('#search').on("input", function () {
+        if ($("#search").val().length > 0) {
+            $(".icon.icon-clear").show();
+        } else {
+            $(".icon.icon-clear").hide();
+        }
+    });
+
+    /*$('#search').on("focus",function(){
+        if($("#search").val().length>0){
+            $(".icon.icon-clear").show();
+        }else{
+            $(".icon.icon-clear").hide();
+        }
+    });
+
+    $('#search').blur(function(){
+        $(".icon.icon-clear").hide();
+    });*/
+
+    $(".icon.icon-clear").click(function () {
+        $("#search").val("");
+        $(this).hide();
+    });
+
+    //时间快捷按钮
+    $(".buttons-row .button").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+    $("#today").click(function () {
+        var myDate = new Date();
+        var todayVal = myDate.format("yyyy-MM-dd");
+        $("#dateStart").val(todayVal);
+        $("#dateEnd").val(todayVal);
+    });
+    $("#yestoday").click(function () {
+        var myDate = new Date();
+        myDate.setTime(myDate.getTime() - 24 * 60 * 60 * 1000);
+        var yestodayVal = myDate.format("yyyy-MM-dd");
+        $("#dateStart").val(yestodayVal);
+        $("#dateEnd").val(yestodayVal);
+    });
+    $("#thisMonth").click(function () {
+        var myDate = new Date();
+        var firstDay = new Date(myDate.getFullYear(), myDate.getMonth(), 1);
+        var lastDay = new Date(myDate.getFullYear(), myDate.getMonth() + 1, 0);
+        var firstDayVal = firstDay.format("yyyy-MM-dd");
+        var lastDayVal = lastDay.format("yyyy-MM-dd");
+        $("#dateStart").val(firstDayVal);
+        $("#dateEnd").val(lastDayVal);
+    });
+    $("#lastMonth").click(function () {
+        var myDate = new Date();
+        var firstDay = new Date(myDate.getFullYear(), myDate.getMonth() - 1, 1);
+        var lastDay = new Date(myDate.getFullYear(), myDate.getMonth(), 0);
+        var firstDayVal = firstDay.format("yyyy-MM-dd");
+        var lastDayVal = lastDay.format("yyyy-MM-dd");
+        $("#dateStart").val(firstDayVal);
+        $("#dateEnd").val(lastDayVal);
+    });
+
+    Date.prototype.format = function (fmt) { //author: meizz
+        var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt))
+            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt))
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
+
+    $("#dateStart,#dateEnd").click(function () {
+        $(".buttons-row").find($(".active")).removeClass("active");
+    });
+
+    //解决键盘遮挡问题
+    var h = $(window).height();
+    window.addEventListener("resize", function () {
+        if ($(window).height() < h) {
+            $('.btnBar').hide();
+        }
+        if ($(window).height() >= h) {
+            $('.btnBar').show();
+        }
+        if (document.activeElement.tagName == "INPUT" || document.activeElement.tagName == "TEXTAREA") {
+            window.setTimeout(function () {
+                document.activeElement.scrollIntoViewIfNeeded();
+            }, 0);
+        }
+    });
+
     $(".back_btn").click(function () {
         var u = navigator.userAgent,
             app = navigator.appVersion;
