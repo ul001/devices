1f22e907d94622e0077276fe2f5b034bcbdf84e4
xuhang <925645182@qq.com>
2020-02-25 09:36:25 +0800

    国际化测试

---
+++ libs/language.js
@@ -0,0 +1,23 @@
+var Operation={};
+
+function getZhLanguage(){
+    Operation={
+        ui_todogoods:'待办事项',
+        ui_address:'地址',
+        ui_photo:'照片',
+        ui_location:'位置',
+        ui_selectSub:'选择变电所',
+        ui_subHolder:'请输入变电所名称或编号'
+    }
+}
+
+function getEnLanguage(){
+    Operation={
+    ui_todogoods:'To-do',
+    ui_address:'Address',
+    ui_photo:'Photo',
+    ui_location:'Location',
+    ui_selectSub:'Select a substation',
+    ui_subHolder:'Please enter the substation name or number',
+    };
+}
