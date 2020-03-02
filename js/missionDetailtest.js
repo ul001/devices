e6c692f0ac524d900caed9b83f88d5e103e8c102
panhong <381500590@qq.com>
2020-03-02 13:56:59 +0800

    轨迹开启 签到 调通

--- js/missionDetailtest.js
+++ js/missionDetailtest.js
@@ -1,13 +1,16 @@
 var u = navigator.userAgent,
-        app = navigator.appVersion;
+    app = navigator.appVersion;
 var isAndroid = u.indexOf("Android") > -1 || u.indexOf("Linux") > -1; //安卓系统
 var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios系统
 
 var isUseTrace = "0";
 //是否有轨迹功能传参
-if(isIOS){
-
-}else if(isAndroid){
+if (isIOS) {
+    window.webkit.messageHandlers.iOS.postMessage("");
+    var storage = localStorage.getItem("accessToken");
+    storage = JSON.parse(storage);
+    isUseTrace = storage.isOpenTrack;
+} else if (isAndroid) {
     isUseTrace = android.getTrackUse();
 }
 
@@ -40,7 +43,7 @@ function getNetData() {
             var userList = data.taskUserList;
             var subLon = taskInfo.fLongitude;
             var subLat = taskInfo.fLatitude;
-            if (taskInfo!=null&&taskInfo!=undefined) {
+            if (taskInfo != null && taskInfo != undefined) {
                 missionsubid = taskInfo.fSubid;
                 $("#missionId").html(taskInfo.fTasknumber);
                 $("#missionType").html(taskInfo.fTasktypeexplain);
@@ -62,67 +65,67 @@ function getNetData() {
                 var temp = false;
                 var thisUser = {};
                 //判断后续
-                if(userList!=undefined && userList.length>0){
+                if (userList != undefined && userList.length > 0) {
                     $(userList).each(function () {
-                    if (this.fUserid == loginUserid) {
-                        temp = true;
-                        thisUser = this;
-                    }
+                        if (this.fUserid == loginUserid) {
+                            temp = true;
+                            thisUser = this;
+                        }
 
-                    var taskStateName = "";
-                    if (this.fExesituation == 7) {
-                        taskStateName = "<span style='color:gray;'>未签到</span>";
-                    } else if (this.fExesituation == 8) {
-                        taskStateName = "<span style='color:blue;'>已签到</span>";
-                    } else if (this.fExesituation == 9) {
-                        taskStateName = "<span style='color:springgreen;'>已提交</span>";
-                    } else {
+                        var taskStateName = "";
+                        if (this.fExesituation == 7) {
+                            taskStateName = "<span style='color:gray;'>未签到</span>";
+                        } else if (this.fExesituation == 8) {
+                            taskStateName = "<span style='color:blue;'>已签到</span>";
+                        } else if (this.fExesituation == 9) {
+                            taskStateName = "<span style='color:springgreen;'>已提交</span>";
+                        } else {
 
-                    }
+                        }
 
-                    var text = "";
-                    text += "<li>";
-                    text +=
-                        '                                <div class="item-content showDiv">';
-                    text +=
-                        '                                    <div class="item-inner">';
-                    text +=
-                        '                                        <div class="item-title label">' +
-                        this.userName +
-                        "</div>";
-                    text +=
-                        '                                        <div class="item-input">';
-                    text +=
-                        '                                            <div class = "item-label" id="input' +
-                        this.fUserid +
-                        '" ';
-                    text +=
-                        '                                                name="number" >' +
-                        taskStateName +
-                        "</div>";
-                    text += "                                        </div>";
-                    text += "                                    </div>";
-                    text += "                                </div>";
-                    text += "                            </li>";
-                    $("#missionState").append(text);
-                });
+                        var text = "";
+                        text += "<li>";
+                        text +=
+                            '                                <div class="item-content showDiv">';
+                        text +=
+                            '                                    <div class="item-inner">';
+                        text +=
+                            '                                        <div class="item-title label">' +
+                            this.userName +
+                            "</div>";
+                        text +=
+                            '                                        <div class="item-input">';
+                        text +=
+                            '                                            <div class = "item-label" id="input' +
+                            this.fUserid +
+                            '" ';
+                        text +=
+                            '                                                name="number" >' +
+                            taskStateName +
+                            "</div>";
+                        text += "                                        </div>";
+                        text += "                                    </div>";
+                        text += "                                </div>";
+                        text += "                            </li>";
+                        $("#missionState").append(text);
+                    });
                 }
 
                 //是执行人
                 //按钮显隐判断
-                if(temp){
+                if (temp) {
                     $("#addVarContain124").show();
-                    if(thisUser.fTaskstarttime==undefined&&missionType=="1"){
+                    if (thisUser.fTaskstarttime == undefined && missionType == "1") {
                         $("#startTask").show();
-                    }else{
-                        if(thisUser.fSignintime==undefined){
+                    } else {
+                        if (thisUser.fSignintime == undefined) {
                             $("#taskIn").show();
-                        }else{
-                            if(thisUser.fCreatetime==undefined){
+                        } else {
+                            if (thisUser.fCreatetime == undefined) {
                                 $("#doTask").show();
                                 $("#submitTask").show();
                                 localStorage.setItem("canClick", "true");
-                            }else{
+                            } else {
                                 $("#textareaDetail").attr("placeholder", "");
                                 $("#textareaDetail").attr("readonly", true);
                                 $("#textareaDetail").val(thisUser.fExplain);
@@ -131,23 +134,23 @@ function getNetData() {
                             }
                         }
                     }
-                }else{
+                } else {
                     localStorage.setItem("canClick", "false");
-                    if(loginUserid==taskchargerid){
+                    if (loginUserid == taskchargerid) {
                         //负责人按钮
-                        if(taskInfo.fTaskfinishdate==undefined){
+                        if (taskInfo.fTaskfinishdate == undefined) {
                             $("#chargeTask").show();
                             $("#chargeSubmit").show();
-                        }else{
+                        } else {
                             $("#doDetail").show();
                         }
-                    }else{
+                    } else {
                         $("#doDetail").show();
                     }
                 }
 
                 //执行人管理按钮
-                if(loginUserid!=taskchargerid && loginUserid!=taskCreatId){
+                if (loginUserid != taskchargerid && loginUserid != taskCreatId) {
                     $("#clickManager").css("display", "none");
                 }
 
@@ -159,11 +162,11 @@ function getNetData() {
                         //缺陷整改
                         localStorage.setItem("taskID", taskID);
                         if (temp) {
-                            if($("#startTask").css("display")!="none"){
+                            if ($("#startTask").css("display") != "none") {
                                 $.toast("请先开启该任务！");
                                 return;
                             }
-                            if($("#taskIn").css("display")!="none"){
+                            if ($("#taskIn").css("display") != "none") {
                                 $.toast("请先现场签到！");
                                 return;
                             }
@@ -179,11 +182,11 @@ function getNetData() {
                         //缺陷整改
                         localStorage.setItem("taskID", taskID);
                         if (temp) {
-                            if($("#startTask").css("display")!="none"){
+                            if ($("#startTask").css("display") != "none") {
                                 $.toast("请先开启该任务！");
                                 return;
                             }
-                            if($("#taskIn").css("display")!="none"){
+                            if ($("#taskIn").css("display") != "none") {
                                 $.toast("请先现场签到！");
                                 return;
                             }
@@ -191,6 +194,65 @@ function getNetData() {
                         window.location.href = "defectRectification.html?value=0";
                     });
                 }
+
+                //现场签到
+                $("#taskIn").click(function () {
+                    $.showPreloader(Operation['ui_loading']);
+                    var loc = "";
+                    if (isIOS) {
+                        window.webkit.messageHandlers.getLocation.postMessage("");
+                        loc = localStorage.getItem("locationStrJS");
+                    } else {
+                        loc = android.getLocation();
+                    }
+                    var lat = "";
+                    var lon = "";
+                    var addr = "";
+                    if (loc == undefined || !loc.length) {
+                        $.hidePreloader();
+                        $.toast("无法获取位置，请检查网络并确保定位授权");
+                        return;
+                    } else if (loc == "-1") {
+                        $.hidePreloader();
+                        $.toast("获取位置超时,建议打开GPS定位服务。");
+                        return;
+                    } else {
+                        $.hidePreloader();
+                    }
+                    if (loc != "" && loc != null) {
+                        var array = loc.split(";");
+                        lat = array[0];
+                        lon = array[1];
+                        addr = array[2];
+                        //                                    alert(lat+"\n"+lon+"\n"+addr);
+                    }
+                    var fDistance = -1;
+                    if (subLat != undefined && subLon != undefined) {
+                        var map = new BMap.Map("allmap");
+                        var point1 = new BMap.Point(subLon, subLat);
+                        var point2 = new BMap.Point(lon, lat);
+                        fDistance = map.getDistance(point1, point2);
+                    }
+                    var param = {
+                        taskId: taskID,
+                        fLongitude: lon,
+                        fLatitude: lat,
+                        fLocation: addr
+                    };
+                    fDistance = parseInt(fDistance);
+                    if (fDistance > 0 && fDistance < 2147483647) {
+                        param['fDistance'] = fDistance;
+                    }
+                    //                            alert(""+taskID+","+lon+","+lat+","+addr);
+                    Substation.postDataByAjax("/taskSingIn", param, function (data) {
+                        $.toast("签到成功！");
+                        localStorage.removeItem("locationStrJS");
+                        $("#taskIn").hide();
+                        $("#doTask").show();
+                        $("#submitTask").show();
+                    });
+                });
+
             }
         }
     );
@@ -199,121 +261,66 @@ function getNetData() {
 getNetData();
 
 //开启任务
-$("#startTask").click(function(){
+$("#startTask").click(function () {
     $.showPreloader(Operation['ui_loading']);
-    Substation.getDataByAjax("/taskStart","taskId="+taskID,function(data){
+    Substation.getDataByAjax("/taskStart", "taskId=" + taskID, function (data) {
         if (isAndroid) {
             android.refresh();
         } else {
             localStorage.setItem("need-refresh", "true");
         }
-        if(isUseTrace=="1"){
+        if (isUseTrace == "1") {
             $.confirm("是否要开启轨迹记录功能？", function () {
                 //开启轨迹
-                if(isIOS){
-
-                }else if(isAndroid){
+                if (isIOS) {
+                    window.webkit.messageHandlers.openTrackFunc.postMessage(null);
+                } else if (isAndroid) {
                     android.startTrace();
                 }
                 location.reload();
-            },function(){location.reload();});
-        }else{
+            }, function () {
+                location.reload();
+            });
+        } else {
             location.reload();
         }
     });
 });
 
-//现场签到
-$("#taskIn").click(function () {
-    $.showPreloader(Operation['ui_loading']);
-    var loc = "";
-    if (isIOS) {
-        window.webkit.messageHandlers.getLocation.postMessage("");
-        loc = localStorage.getItem("locationStrJS");
-    } else {
-        loc = android.getLocation();
-    }
-    var lat = "";
-    var lon = "";
-    var addr = "";
-    if (loc == undefined || !loc.length) {
-        $.hidePreloader();
-        $.toast("无法获取位置，请检查网络并确保定位授权");
-        return;
-    } else if (loc == "-1") {
-        $.hidePreloader();
-        $.toast("获取位置超时,建议打开GPS定位服务。");
-        return;
-    } else {
-        $.hidePreloader();
-    }
-    if (loc != "" && loc != null) {
-        var array = loc.split(";");
-        lat = array[0];
-        lon = array[1];
-        addr = array[2];
-//                                    alert(lat+"\n"+lon+"\n"+addr);
-    }
-    var fDistance = -1;
-    if (subLat != undefined && subLon != undefined) {
-        var map = new BMap.Map("allmap");
-        var point1 = new BMap.Point(subLon, subLat);
-        var point2 = new BMap.Point(lon, lat);
-        fDistance = map.getDistance(point1, point2);
-    }
-    var param = {
-        taskId: taskID,
-        fLongitude: lon,
-        fLatitude: lat,
-        fLocation: addr
-    };
-    fDistance = parseInt(fDistance);
-    if (fDistance > 0 && fDistance < 2147483647) {
-        param['fDistance'] = fDistance;
-    }
-    //                            alert(""+taskID+","+lon+","+lat+","+addr);
-    Substation.postDataByAjax("/taskSingIn", param, function (data) {
-        $.toast("签到成功！");
-        localStorage.removeItem("locationStrJS");
-        $("#taskIn").hide();
-        $("#doTask").show();
-        $("#submitTask").show();
-    });
-});
 
 //执行任务按钮事件
 $(".doDetail").click(function () {
-        localStorage.setItem("fSubid", missionsubid);
-        localStorage.setItem("taskID", taskID);
-        if($(this).attr("id")=="doTask"){
-            localStorage.setItem("canClick","true");
-        }else{
-            localStorage.setItem("canClick","false");
-        }
-        if (missionTypeid == 1) {
-            localStorage.setItem("fPlacecheckformid", placeCheckFormId);
-            if($(this).attr("id")=="doTask"){
-                var attion = Operation['ui_attention'];
-                var attiondetail = Operation['ui_executionprompt'];
-                $.confirm(
-                    attiondetail,
-                    attion,
-                    function () {
-                        window.location.href = "patrolContent.html";
-                    }
-                );
-            } else {
-                window.location.href = "patrolContent.html";
-            }
-        } else if (missionTypeid == 2) {
-            //现场交接任务
-            window.location.href = "missionScene.html";
-        } else if (missionTypeid == 3) {
-            //缺陷整改
-            window.location.href = "defectRectification.html";
+    localStorage.setItem("fSubid", missionsubid);
+    localStorage.setItem("taskID", taskID);
+    if ($(this).attr("id") == "doTask") {
+        localStorage.setItem("canClick", "true");
+    } else {
+        localStorage.setItem("canClick", "false");
+    }
+    if (missionTypeid == 1) {
+        localStorage.setItem("fPlacecheckformid", placeCheckFormId);
+        if ($(this).attr("id") == "doTask") {
+            var attion = Operation['ui_attention'];
+            var attiondetail = Operation['ui_executionprompt'];
+            $.confirm(
+                attiondetail,
+                attion,
+                function () {
+                    window.location.href = "patrolContent.html";
+                }
+            );
         } else {
-            $.toast("未知任务类型");
+            window.location.href = "patrolContent.html";
         }
+    } else if (missionTypeid == 2) {
+        //现场交接任务
+        window.location.href = "missionScene.html";
+    } else if (missionTypeid == 3) {
+        //缺陷整改
+        window.location.href = "defectRectification.html";
+    } else {
+        $.toast("未知任务类型");
+    }
 });
 
 //提交按钮事件
@@ -343,7 +350,7 @@ $("#submitTask").click(function () {
 });
 
 //负责人提交任务
-$("#chargeSubmit").click(function(){
+$("#chargeSubmit").click(function () {
     $.confirm("确定要提交并结束任务吗？", function () {
         var param;
         param = {
@@ -371,9 +378,9 @@ $(".pull-left.click_btn").click(function () {
 
 //管理页面
 $("#clickManager").click(function () {
-/*    localStorage.setItem("fSubname", "执行情况");
-    localStorage.setItem("missionSubid", missionsubid);
-    localStorage.setItem("missionPlaceCheckFormId", placeCheckFormId);*/
+    /*    localStorage.setItem("fSubname", "执行情况");
+        localStorage.setItem("missionSubid", missionsubid);
+        localStorage.setItem("missionPlaceCheckFormId", placeCheckFormId);*/
     localStorage.setItem("taskID", taskID);
     if (taskCreatId != loginUserid && missionType != "3") {
         localStorage.setItem("hiddenBtn", "NO");
