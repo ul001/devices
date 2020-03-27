88aec6b5770ca93c1c1e9cefc2db4e519a1537bb
xuhang <925645182@qq.com>
2020-03-27 10:52:52 +0800

    版本1.2.7

--- libs/tools.js
+++ libs/tools.js
@@ -4,7 +4,7 @@
  * @description 存放常用工具类
  */
 var baseUrlFromAPP = "http://116.236.149.165:8090/SubstationWEBV2/v4";
-var tokenFromAPP = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODUxMTk5NzEsInVzZXJuYW1lIjoiaGFoYWhhIn0.WmaU6BGz7QRGxuIafXugEK0UkvA5WsHlcXhGXADzoRU";
+var tokenFromAPP = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODU3NDE4MDAsInVzZXJuYW1lIjoiaGFoYWhhIn0.lN6fA645l5Pi7UQRSjTrLak-17RhnmGDyI00fBJPM4o";
 var ipAddress = "http://116.236.149.165:8090";
 var userId = "315";
 //语言字段传参
