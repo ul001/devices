3cb24f14895215d2a0039161db2b85aaccbddd35
panhong <381500590@qq.com>
2020-04-14 19:36:39 +0800

    增加funcid

--- js/lightingDetail.js
+++ js/lightingDetail.js
@@ -146,7 +146,7 @@ function creatView(param) {
 }
 
 $("#record_btn").click(function () {
-    window.location.href = 'deviceControlLog.html?type=light&meterId=' + param.fMetercode + '&meterId=' + param.datafuncid + '';
+    window.location.href = 'deviceControlLog.html?type=light&meterId=' + param.fMetercode + '&funcid=' + param.datafuncid + '';
 });
 
 $.init();
