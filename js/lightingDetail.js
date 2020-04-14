7709409634ee3c45dee592871c7ea9935874eb44
panhong <381500590@qq.com>
2020-04-14 19:33:42 +0800

    增加传参

--- js/lightingDetail.js
+++ js/lightingDetail.js
@@ -146,7 +146,7 @@ function creatView(param) {
 }
 
 $("#record_btn").click(function () {
-    window.location.href = 'deviceControlLog.html?type=light&meterId=' + param.fMetercode + '';
+    window.location.href = 'deviceControlLog.html?type=light&meterId=' + param.fMetercode + '&meterId=' + param.datafuncid + '';
 });
 
 $.init();
