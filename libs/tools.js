6a0dd32553cceb22404b2e61d22608fe7f3df95d
xuhang <925645182@qq.com>
2020-07-02 09:01:04 +0800

    Android提示框更改

--- libs/tools.js
+++ libs/tools.js
@@ -4,7 +4,7 @@
  * @description 存放常用工具类
  */
 var baseUrlFromAPP = "http://116.236.149.165:8090/SubstationWEBV2/v5";
-var tokenFromAPP = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTI4NzkzOTcsInVzZXJuYW1lIjoiaGFoYWhhIn0.6ZYFYdQbzswWlUk6TZy2xSV7R4MZ5BdsQcUACKvIjE8";
+var tokenFromAPP = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTM5MTYxMTUsInVzZXJuYW1lIjoiaGFoYWhhIn0.lLzdJwieIO-xMhob6PW06MRyzK4oCZVCfcs9196Iec8";
 var ipAddress = "http://116.236.149.165:8090";
 var userId = "315";
 //语言字段传参
