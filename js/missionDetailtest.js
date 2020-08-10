e01ced724db7372098bb59a375b3e306c72fd467
panhong <381500590@qq.com>
2020-08-10 10:17:36 +0800

    尝试解决安卓跳转问题，搭建missiontimeaxis框架

--- js/missionDetailtest.js
+++ js/missionDetailtest.js
@@ -837,11 +837,12 @@ function gotoMissionTimeAxis(userid) {
     // localStorage.setItem("missionTypeName", missionTypeName);
     localStorage.setItem("subLat", subLat);
     localStorage.setItem("subLon", subLon);
-    // if (isAndroid) {
-    //     android.goToInHtml("missionTimeAxis.html");
-    // } else {
-    window.location.href = "missionTimeAxis.html";
-    // }
+    if (isAndroid) {
+        // android.goToInHtml("missionTimeAxis.html");
+        android.goToInAxis();
+    } else {
+        window.location.href = "missionTimeAxis.html";
+    }
 }
 
 
