3256927d51a434b3e9f1cabb11c8cfb1c090c9cf
panhong <381500590@qq.com>
2020-11-04 14:11:44 +0800

    分合闸控制功能+视频缓存播放

--- js/versionHistoryView.js
+++ js/versionHistoryView.js
@@ -2,120 +2,123 @@ var historyLog = localStorage.getItem("updateLog");
 var videoSrc = localStorage.getItem("videoUrl"); //新的视频播放地址
 var appId = "ab55ce55Ac213hlkhl23419f179c5f6f";
 if (isAndroid) {
-    appId = "ab55ce55Ac213hlkhl23419f179c5f6f";
+  appId = "ab55ce55Ac213hlkhl23419f179c5f6f";
 } else if (isIOS) {
-    appId = "iose70eeb320a58230925c02e7";
+  appId = "iose70eeb320a58230925c02e7";
 }
 
 var jumpId = Substation.GetQueryString("jumpId");
 var isPush = "0";
 if (jumpId != undefined && jumpId != null && jumpId != "") {
-    isPush = "1";
+  isPush = "1";
 }
 
 if (isPush == "0" && historyLog) {
-    //配置说明
-    historyLog = JSON.parse(historyLog);
-    var reg = new RegExp("<br>", "g"); //g,表示全部替换。
-    historyLog.replace(reg, "/n");
-    //配置视频
-    if (videoSrc == undefined || videoSrc == "" || videoSrc == null) {
-        $("#videoRoom").remove();
-        $("#updateLog").css("height", "calc(100% - 10px)");
-    } else {
-        document.getElementById("videoplay").src = videoSrc;
-        document.getElementById("videoplay").play();
-        var msgDic = {
-            videoUrl: videoSrc
-        };
-        window.webkit.messageHandlers.downloadVideo.postMessage(msgDic);
-    }
-    $("#updateLog").html(historyLog);
-    //    document.getElementById("videoplay").play();
+  //配置说明
+  historyLog = JSON.parse(historyLog);
+  var reg = new RegExp("<br>", "g"); //g,表示全部替换。
+  historyLog.replace(reg, "/n");
+  //配置视频
+  if (videoSrc == undefined || videoSrc == "" || videoSrc == null) {
+    $("#videoRoom").remove();
+    $("#updateLog").css("height", "calc(100% - 10px)");
+  } else {
+    document.getElementById("videoplay").src = videoSrc;
+    document.getElementById("videoplay").play();
+    var msgDic = {
+      videoUrl: videoSrc
+    };
+    window.webkit.messageHandlers.downloadVideo.postMessage(msgDic);
+  }
+  $("#updateLog").html(historyLog);
+  //    document.getElementById("videoplay").play();
 } else {
-    getData();
+  getData();
+}
+
+function changePlayStr(src) {
+  document.getElementById("videoplay").src = src;
+  document.getElementById("videoplay").type = "video/mp4";
+  //   document.getElementById("videoplay").play();
 }
 
 function getData() {
-    $.ajax({
-        type: "GET",
-        url: "http://www.acrelcloud.cn/SubstationWEBV2/sys/getVersionHistory",
-        data: {
-            fId: appId
-        },
-        beforeSend: function (request) {
-            // request.setRequestHeader("Authorization", localStorage.getItem("Authorization"));
-            //                        request.setRequestHeader("Authorization", tokenFromAPP);
-            $.showPreloader(Operation['ui_loading']);
-        },
-        success: function (data) {
-            $.hidePreloader();
-            if (data == undefined) {
-
-                return;
-            } else {
-                if (data.code == "200") {
-
-                    if (data.data == null || data.data == "" || data.data == undefined) {
-
-                    } else {
-                        videoUrl = 'http://www.acrelcloud.cn/' + data.data.fileURL + '/';
-                        if (data.data.list != undefined && data.data.list.length > 0) {
-                            var showDate = data.data.list[0];
-                            historyLog = showDate.fUpdatelog;
-                            videoSrc = videoUrl + showDate.fDemofile;
-                            //配置说明
-                            // historyLog = JSON.parse(historyLog);
-                            var reg = new RegExp("<br>", "g"); //g,表示全部替换。
-                            historyLog.replace(reg, "/n");
-                            //配置视频
-                            if (videoSrc == undefined || videoSrc == "" || videoSrc == null) {
-                                $("#videoRoom").remove();
-                                $("#updateLog").css("height", "calc(100% - 10px)");
-                            } else {
-                                document.getElementById("videoplay").src = videoSrc;
-                                document.getElementById("videoplay").play();
-                                var msgDic = {
-                                    videoUrl: videoSrc
-                                };
-                                window.webkit.messageHandlers.downloadVideo.postMessage(msgDic);
-                            }
-                            $("#updateLog").html(historyLog);
-                        }
-                    }
-                } else if (data.code == "5000") {
-                    Substation.showCodeTips(data.code);
-                    Substation.reportError(JSON.stringify(data.data.stackTrace));
-                } else {
-                    Substation.showCodeTips(data.code);
-                }
+  $.ajax({
+    type: "GET",
+    url: "http://www.acrelcloud.cn/SubstationWEBV2/sys/getVersionHistory",
+    data: {
+      fId: appId
+    },
+    beforeSend: function(request) {
+      // request.setRequestHeader("Authorization", localStorage.getItem("Authorization"));
+      //                        request.setRequestHeader("Authorization", tokenFromAPP);
+      $.showPreloader(Operation["ui_loading"]);
+    },
+    success: function(data) {
+      $.hidePreloader();
+      if (data == undefined) {
+        return;
+      } else {
+        if (data.code == "200") {
+          if (data.data == null || data.data == "" || data.data == undefined) {
+          } else {
+            videoUrl = "http://www.acrelcloud.cn/" + data.data.fileURL + "/";
+            if (data.data.list != undefined && data.data.list.length > 0) {
+              var showDate = data.data.list[0];
+              historyLog = showDate.fUpdatelog;
+              videoSrc = videoUrl + showDate.fDemofile;
+              //配置说明
+              // historyLog = JSON.parse(historyLog);
+              var reg = new RegExp("<br>", "g"); //g,表示全部替换。
+              historyLog.replace(reg, "/n");
+              //配置视频
+              if (videoSrc == undefined || videoSrc == "" || videoSrc == null) {
+                $("#videoRoom").remove();
+                $("#updateLog").css("height", "calc(100% - 10px)");
+              } else {
+                document.getElementById("videoplay").src = videoSrc;
+                // document.getElementById("videoplay").play();
+                var msgDic = {
+                  videoUrl: videoSrc
+                };
+                window.webkit.messageHandlers.downloadVideo.postMessage(msgDic);
+              }
+              $("#updateLog").html(historyLog);
             }
-        },
-        error: function (data) {
-            if (data.status == 0) {
-                $.toast(Operation['ui_neterror']);
-            } else {
-                $.toast(Operation['code_fail']);
-            }
-        },
-        complete: function () {
-            $.hidePreloader();
+          }
+        } else if (data.code == "5000") {
+          Substation.showCodeTips(data.code);
+          Substation.reportError(JSON.stringify(data.data.stackTrace));
+        } else {
+          Substation.showCodeTips(data.code);
         }
-    });
+      }
+    },
+    error: function(data) {
+      if (data.status == 0) {
+        $.toast(Operation["ui_neterror"]);
+      } else {
+        $.toast(Operation["code_fail"]);
+      }
+    },
+    complete: function() {
+      $.hidePreloader();
+    }
+  });
 }
 
-$(".back_btn").click(function () {
-    localStorage.removeItem("historyLog");
-    localStorage.removeItem("videoUrl");
-    if (isPush == "1") {
-        //推送详情点击返回事件
-        if (isAndroid) {
-            android.goBack();
-        } else if (isIOS) {
-            //            window.history.back();
-            window.webkit.messageHandlers.goBackiOS.postMessage("");
-        }
-    } else {
-        window.history.back();
+$(".back_btn").click(function() {
+  localStorage.removeItem("historyLog");
+  localStorage.removeItem("videoUrl");
+  if (isPush == "1") {
+    //推送详情点击返回事件
+    if (isAndroid) {
+      android.goBack();
+    } else if (isIOS) {
+      //            window.history.back();
+      window.webkit.messageHandlers.goBackiOS.postMessage("");
     }
+  } else {
+    window.history.back();
+  }
 });
